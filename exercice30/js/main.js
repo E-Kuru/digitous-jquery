00374fa011f2ce7e937a6af4827c02272546cc6b
E-Kuru <evandalleaublop@gmail.com>
2021-10-18 17:55:31 +0200

    Fourth part done

---
+++ exercice30/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $('input').keyup(function(){
+        $('input').removeClass('is-invalid')
+        $('input').removeClass('is-valid')
+    if($('input').val().length >= 5){
+        $('input').addClass('is-valid')
+    }
+    else{
+        $('input').addClass('is-invalid')
+    }
+    })
+
+})
