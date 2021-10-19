a16e17a389a19bd9ac3459b1616e9cf69e814c03
E-Kuru <evandalleaublop@gmail.com>
2021-10-19 17:08:47 +0200

    Search bar done

--- got-api/index.js
+++ got-api/index.js
@@ -22,15 +22,32 @@ $.ajax({
 })
 
 function getCharacter(){
-    var name = document.getElementById('name')
-    console.log(name.value);
+
+    var input = document.getElementById('name').value
+
     $.ajax({
         url: 'https://thronesapi.com/api/v2/Characters',
         success: function(data) {
             console.log(data);
-            data.filter(function(e){
-                return e.fullName === 'Jon Snow'
+            var filterdName = data.filter(function(charName){
+                return charName.fullName.includes(input)
             })
+            console.log(filterdName);
+            $('#characters').html('')
+            filterdName.forEach(function (element) {
+                $('#characters').append(
+                    `
+                <div class="card" style="width: 18rem;">
+                <img src="${element.imageUrl}" class="card-img-top">
+                <div class="card-body">
+                  <h3 class="card-title">${element.fullName}</h5>
+                  <h5 class="card-title">${element.title}</h5>
+                </div>
+              </div>
+              `
+                )
+            });
+    
         }
     })
 }
