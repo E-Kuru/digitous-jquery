89644a1de09d4ea4a02c4d58ae2ca83d07abcc71
E-Kuru <evandalleaublop@gmail.com>
2021-10-19 17:11:32 +0200

    reset button

--- got-api/index.js
+++ got-api/index.js
@@ -21,6 +21,28 @@ $.ajax({
 });
 })
 
+function reset (){
+    $.ajax({
+        url: 'https://thronesapi.com/api/v2/Characters',
+        success: function(e) {
+            // console.log(e);
+            e.forEach(function (element) {
+                $('#characters').append(
+                    `
+                <div class="card" style="width: 18rem;">
+                <img src="${element.imageUrl}" class="card-img-top">
+                <div class="card-body">
+                  <h3 class="card-title">${element.fullName}</h5>
+                  <h5 class="card-title">${element.title}</h5>
+                </div>
+              </div>
+              `
+                )
+            });
+        }
+    });
+}
+
 function getCharacter(){
 
     var input = document.getElementById('name').value
